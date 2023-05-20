0194cf102ec5f1010202c18c1d1128d8c439cbee
rafael alves <rafaelc.santos@sicoob.com.br>
2023-05-19 21:59:29 -0300

    add function to choose photo

--- src/app/photoGallery/photoGallery.page.ts
+++ src/app/photoGallery/photoGallery.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; 
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
+
 
 @Component({
-  selector: 'app-tab1',
+  selector: 'app-photo-gallery',
   templateUrl: 'photoGallery.page.html',
   styleUrls: ['photoGallery.page.scss']
 })
 export class PhotoGalleryPage {
+  photos: string[] = [];
 
   constructor() {}
+ 
+  async choosePhoto() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Photos,
+    });
 
+    this.photos.push(image.dataUrl!);
+  }
+
+  deletePhoto(index: number) {
+    this.photos.splice(index, 1);
+  }
 }
+
+
